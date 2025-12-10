4441c97b5bcdc76e7df4a4ddbd1e4dfa8754cd5e
jfremag <jfremag@gmail.com>
2025-12-10 21:18:52 +0100

    Add theme administration UI

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 const ProcessList = () => import('../components/ProcessList.vue');
 const ProcessEditor = () => import('../components/ProcessEditor.vue');
 const ProcessViewer = () => import('../components/ProcessViewer.vue');
+const ThemeAdmin = () => import('../components/ThemeAdmin.vue');
 
 const routes = [
   { path: '/', name: 'home', component: ProcessList },
   { path: '/editor/:id', name: 'editor', component: ProcessEditor },
   { path: '/viewer/:id', name: 'viewer', component: ProcessViewer },
+  { path: '/admin/themes', name: 'theme-admin', component: ThemeAdmin },
 ];
 
 const router = createRouter({
